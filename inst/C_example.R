1fb3c03e2f509ea7a8a83f1031debe4b9e955438
richard <glennie@glennies.co.uk>
2019-12-09 08:49:35 +0000

    clean up example scripts

---
+++ inst/C_example.R
@@ -0,0 +1,11 @@
+# Running C_example.cpp to optimize a function in C++
+library(RcppEigen)
+library(Rcpp)
+# compile function
+sourceCpp("C_example.cpp")
+
+# Run optimization
+res <- doF(c(0, 0), hessupdate = 10, verbose = TRUE)
+
+# Ouputs
+res
